22a22d58db3ab3a743d923894c5f10ef97e98c9a
marcio <marcio.heleno@gmail.com>
2019-07-18 19:14:02 -0300

    ajuste inicial no repositorio

---
+++ src/main/java/website/marcioheleno/pontodigital/PontoDigitalApplication.java
@@ -0,0 +1,13 @@
+package website.marcioheleno.pontodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoDigitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PontoDigitalApplication.class, args);
+    }
+
+}
